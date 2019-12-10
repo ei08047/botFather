01ec52ac1b4f4c5dc84644d0d9c9946bbaa95315
Jose Ferreira <ze@Air-de-Jose.lan>
2019-12-10 01:07:01 +0000

    optimized build

---
+++ build/precache-manifest.9b8c4e085c6703484c51fff331b69cf9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a42d537d5fea78bc461430c3f952830",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae286276a33834098832",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b2ca196859900848fdf0",
+    "url": "/static/js/2.64477202.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.64477202.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ae286276a33834098832",
+    "url": "/static/js/main.acd0cb6e.chunk.js"
+  },
+  {
+    "revision": "511b8e2ebfdce74f81b8",
+    "url": "/static/js/runtime-main.f27ea392.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
